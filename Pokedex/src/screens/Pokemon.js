c527aad7d15b588fa0c587eaba151aa10f7ab702
Alfonso Ramírez <arammes03@gmail.com>
2023-10-14 12:13:32 +0200

    boton de añadir pokemon a favoritos

--- Pokedex/src/screens/Pokemon.js
+++ Pokedex/src/screens/Pokemon.js
@@ -6,6 +6,8 @@ import { getPokemonDetailsApi } from "../api/pokemon";
 import Header from "../components/Pokemon/Header";
 import Type from "../components/Pokemon/Type";
 import Stats from "../components/Pokemon/Stats";
+import Favorite from "../components/Pokemon/Favorite";
+import useAuth from "../hooks/useAuth";
 
 export default function Pokemon(props) {
   const {
@@ -14,10 +16,11 @@ export default function Pokemon(props) {
   } = props;
 
   const [pokemon, setPokemon] = useState(null);
+  const { auth } = useAuth();
 
   useEffect(() => {
     navigation.setOptions({
-      headerRight: () => null,
+      headerRight: () => (auth ? <Favorite id={pokemon?.id} /> : null),
       headerLeft: () => (
         <Icon
           name="arrow-left"
@@ -28,7 +31,7 @@ export default function Pokemon(props) {
         />
       ),
     });
-  }, [navigation, params]);
+  }, [navigation, params, pokemon]);
 
   useEffect(() => {
     (async () => {
@@ -42,6 +45,7 @@ export default function Pokemon(props) {
   }, [params]);
 
   if (!pokemon) return null;
+
   return (
     <ScrollView>
       <Header
