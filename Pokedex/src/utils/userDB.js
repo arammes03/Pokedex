2f23803dbc8751371e0990daed6a6a798481b914
arammes118 <137161895+arammes03@users.noreply.github.com>
2023-10-14 11:26:10 +0200

    datos de usuario

---
+++ Pokedex/src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "alfonso03",
+  password: "123456",
+};
+
+export const userDetails = {
+  username: "alfonso03",
+  firstName: "Alfonso",
+  lastName: "Ramírez Mestanza",
+  email: "arammes03@gmail.com",
+};
