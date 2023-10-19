8c03b665b9c13945216bfb37b9500321e8244fbc
arammes118 <137161895+arammes03@users.noreply.github.com>
2023-10-19 21:59:38 +0200

    contador de pokemons

--- Pokedex/src/components/Auth/UserData.js
+++ Pokedex/src/components/Auth/UserData.js
@@ -1,9 +1,26 @@
 import { View, Text, StyleSheet, Button } from "react-native";
-import React from "react";
+import React, { useState, useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import { set, size } from "lodash";
 import useAuth from "../../hooks/useAuth";
+import { getPokemonsFavoriteApi } from "../../api/favorite";
 
 export default function UserData() {
   const { auth, logout } = useAuth();
+  const [total, setTotal] = useState(0);
+
+  useFocusEffect(
+    useCallback(() => {
+      (async () => {
+        try {
+          const response = await getPokemonsFavoriteApi();
+          setTotal(size(response));
+        } catch (error) {
+          setTotal(0);
+        }
+      })();
+    }, [])
+  );
 
   return (
     <View style={styles.content}>
@@ -19,7 +36,7 @@ export default function UserData() {
         ></ItemMenu>
         <ItemMenu title="Username" text={auth.username} />
         <ItemMenu title="Email" text={auth.email} />
-        <ItemMenu title="Total Favoritos" text={`0 Pokemons`} />
+        <ItemMenu title="Total Favoritos" text={`${total} pokemons`} />
       </View>
       <Button title="Desconectarse" onPress={logout} />
     </View>
