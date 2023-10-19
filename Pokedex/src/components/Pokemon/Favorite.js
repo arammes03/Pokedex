4f3313e249ad084f86df7d5855450f967f55d2c1
arammes118 <137161895+arammes03@users.noreply.github.com>
2023-10-19 21:12:10 +0200

    Reload a tiempo real del icono al añadir un nuevo pokemon a favs

--- Pokedex/src/components/Pokemon/Favorite.js
+++ Pokedex/src/components/Pokemon/Favorite.js
@@ -12,6 +12,8 @@ export default function Favorite(props) {
 
   // Estado que guarda si un pokemon ya está en la lista de favoritos
   const [isFavorite, setIsFavorite] = useState(undefined);
+  // Estado que actua de interructor para refrescar en tiempo real el icono de favs del pokemon
+  const [reloadCheck, setReloadCheck] = useState(false);
 
   // Constante del icono que dependiendo del estado del pokemon (fav/no fav), sale solo el borde o sólido
   const Icon = isFavorite ? FontAwesome : FontAwesome5;
@@ -25,11 +27,21 @@ export default function Favorite(props) {
         setIsFavorite(false);
       }
     })();
-  }, [id]);
+  }, [id, reloadCheck]);
+
+  // Constante que permitirá "refrescar" la página para actualizar el icono de favs
+  const onReloadCheck = () => {
+    setReloadCheck((prev) => !prev); // Seteara el estado contrario de nuestra constante reloadCheck
+  };
 
   // Constante que llama a nuestra función de añadir pokemon a favs
   const addFavorite = async () => {
-    await addPokemonFavoriteApi(id); // Llamada a la función con el id del pokemon elegido
+    try {
+      await addPokemonFavoriteApi(id); // Llamada a la función con el id del pokemon elegido
+      onReloadCheck(); // Ejecutamos el reload para el cambio de icono
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   // Constante que llama a nuestra función de eliminar pokemon de favs
